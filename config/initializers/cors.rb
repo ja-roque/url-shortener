a456fb0c7bfe67fb36939cbf97fd7d4d77de2c49
Javier Roque <jarb95@gmail.com>
2019-04-25 12:02:39 -0600

    Added AWS url just in case.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:3000', 'www.cuturl.work']
+    origins ['localhost:3000', 'www.cuturl.work', 'www.cuturl.work.s3-website.us-east-2.amazonaws.com']
 
     resource '*',
       headers: :any,
